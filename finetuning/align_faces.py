6d7503147eb0ce535a36a92c5e887f71a61a3ab5
JohnvadL <john.vadayattukunnellal@mail.utoronto.ca>
2020-12-14 01:08:00 -0500

    Finish pretraining and fix align_faces for finetuning

--- finetuning/align_faces.py
+++ finetuning/align_faces.py
@@ -1,3 +1,11 @@
+import os
+
+import cv2
+import dlib
+import numpy as np
+from PIL import Image
+
+
 class align_faces(object):
 
     def __call__(self, image):
@@ -9,23 +17,22 @@ class align_faces(object):
         LEFT_EYE_CORD = (0.25, 0.2)
         DIMENSIONS = 244
 
-        train_folder = '/content/drive/My Drive/training/'
+        train_folder = os.getcwd()
         predictor_path = os.path.join(train_folder, "shape_predictor_5_face_landmarks.dat")
-        detector_path = os.path.join(train_folder, "mmod_human_face_detector.dat")
+        # detector_path = os.path.join(train_folder, "mmod_human_face_detector.dat")
         shape_predictor = dlib.shape_predictor(predictor_path)
-        face_detector = dlib.cnn_face_detection_model_v1(detector_path)
+        face_detector = dlib.get_frontal_face_detector()
 
+        # changed here for efficiency
+        # face_detector = dlib.cnn_face_detection_model_v1(detector_path)
         faces = face_detector(image)
 
         if not faces:
             raise ValueError("Image has no detectable faces")
 
         # assumption is made that there is only one
-        for face_rect in faces:
-            face = face_rect.rect
+        for face in faces:
             landmarks = shape_predictor(image, face)
-
-            # iterating and converting from points object due to limitations
             landmarks = landmarks.parts()
             landmarks = self.convert_to_np(landmarks)
 
@@ -34,8 +41,11 @@ class align_faces(object):
             minimum = np.min(landmarks, axis=0)
 
             # eye landmarks
-            left = landmarks[:1]
-            right = landmarks[1:2]
+            right = landmarks[:2]
+            left = landmarks[2:4]
+
+            left = np.mean(left, axis=0, dtype=np.int)
+            right = np.mean(right, axis=0, dtype=np.int)
 
             centre = np.vstack((left, right))
             centre = np.mean(centre, axis=0, dtype=np.int)
@@ -47,7 +57,7 @@ class align_faces(object):
 
             # find the length of the face, and use that for our scale
             y_scale = maximum[1] - minimum[1]
-            y_scale = y_scale + 0.9 * y_scale
+            y_scale = y_scale + 2.1 * y_scale
 
             M = cv2.getRotationMatrix2D((centre[0], centre[1]), angle, DIMENSIONS / y_scale)
 
@@ -62,7 +72,6 @@ class align_faces(object):
 
             # convert back to PIL
             return Image.fromarray(image2)
-
 
     @staticmethod
     def convert_to_np(points):
